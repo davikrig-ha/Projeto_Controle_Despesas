b1c5209af543b6fafdaf43e123f1c29e1552460d
Gabriel-Amaro-04 <lipebehling@gmail.com>
2021-09-21 21:17:50 -0300

    javascript constante

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const dummyTransactions = [
+    {id : 1, name : 'Bolo de brigadeiro', amount: -20},
+    {id : 2, name : 'Salario', amount: 30},
+    {id : 3, name : 'Bolo de Limao', amount: -20},
+    {id : 3, name : 'Violao', amount: 150}
+]
+
+const addTransactionIntoDom = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+' 
+
+    console.log(operator);
+}
+
+addTransactionIntoDom(dummyTransactions[1]);
