b52f68aaa035b8c6408e6dc95d556466c1fd174c
Daví Luiz <83046706+davikrig-ha@users.noreply.github.com>
2021-09-22 21:24:46 -0300

    aula quarta-feira

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const transactionsUl = document.querySelector('transactions')
+const transactionsUl = document.querySelector('#transactions') 
 
 
 
@@ -10,9 +10,39 @@ const dummyTransactions = [
 ]
 
 const addTransactionIntoDom = transaction => {
-    const operator = transaction.amount < 0 ? 'minus' : 'plus'; 
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const CSSclass = transaction.amount < 0 ? 'minus' : 'plus';
+    const li = document.createElement('li');
+    li.classList.add(CSSclass);
 
-    console.log(operator);
+    li.innerHTML = `
+    ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator} </span> 
+    <button class="delete-btn">x</button>
+    `
+
+    transactionsUl.append(li);
 }
 
-addTransactionIntoDom(dummyTransactions[1]);
+const updateBalanceValues = () => {
+    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
+
+
+    const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0).toFixed(2);
+
+
+    const income = transactionAmounts.filter(values => values > 0).reduce((acumulator, values) => acumulator + values,0).toFixed(2);
+
+    const expense = Math.abs(transactionAmounts.filter(value => value < 0).reduce((acumulator, value) => acumulator + value, 0)).toFixed(2);
+}
+
+
+
+
+const init = () =>{
+    dummyTransactions.forEach(addTransactionIntoDom);
+    updateBalanceValues();
+
+}
+
+init();
