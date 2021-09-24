b385d5fa3836b4a5b03fd40f1205d9adc6ca051d
Daví Luiz <83046706+davikrig-ha@users.noreply.github.com>
2021-09-24 19:37:55 -0300

    projeto quase pronto

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const moneyMinusDisplay = document.querySelector('#money-minus');
 
 const balanceDisplay = document.querySelector('#balance'); 
 
-const inputTransactionName = document.querySelector('#name'); 
+const inputTransactionName = document.querySelector('#text'); 
 
 const inputTransactionAmount = document.querySelector('#amount'); 
 
@@ -35,6 +35,9 @@ const addTransactionIntoDom = transaction => {
 }
 
 const updateBalanceValues = () => {
+
+    //atualiza os valores do objeto
+
     const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
 
     const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0).toFixed(2);
@@ -50,6 +53,7 @@ const updateBalanceValues = () => {
 
 
 
+//preenche a pagina
 
 const init = () =>{
     dummyTransactions.forEach(addTransactionIntoDom);
@@ -58,3 +62,32 @@ const init = () =>{
 }
 
 init();
+
+
+//gera um id
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+   
+    event.preventDefault();
+
+     /* Ouve o submit do form para verificar se inputs estão
+        preenchidos e adicionar no array de transações */
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+  
+    if (transactionName === '' || transactionAmount === '')
+    { alert('Por favor, Informe os valores e o nome da transação!'); 
+    return}
+    
+    // Cria o objeto
+    const transaction = { 
+        id: generateID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount)
+      }    
+
+
+});
